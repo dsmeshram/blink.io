60705d5f5b4d61a0bfb51f4289d6d516ab250ca6
Damodhar M <damodhar.meshram.a1@gmail.com>
2023-12-05 10:51:07 +0530

    on boardign page with proper card and images

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 import React from 'react'
-import {Card, CardHeader, CardBody, Image, Button, CardFooter, User} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, Image, Button, CardFooter, User, Checkbox} from "@nextui-org/react";
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
+
+import type1 from "../images/1.png"
+import type2 from "../images/2.png"
+import type3 from "../images/3.png"
 
 const OnBoardingPage = () => {
     const { data: session } = useSession()
     const router = useRouter();
+
+    const [type, setType] = React.useState(2); 
   return (
     <>
     <div className='text-center p-4 '>
@@ -29,10 +35,22 @@ const OnBoardingPage = () => {
 
                     />
       </div>
-    <div className='p-60  gap-4 flex justify-center'>
+    <div className='p-40  gap-4  justify-center xl:grid-cols-3 flex sm:grid-cols-1'>
     <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-large">For yourself</h4>
+    <Checkbox className='pl-4 pt-4' isSelected={type == 1} onValueChange={()=>setType(1)}></Checkbox>
+
+      <CardHeader className="pb-0 pt-2 px-4 flex-col justify-center items-center">
+      <Image
+        alt="Woman listing to music"
+        className="object-cover  m-5 h-32 justify-center"
+        isZoomed
+        isBlurred
+        height={50}
+        src={type1.src}
+      />
+        <h4 className="font-bold text-large pt-4">For You</h4>
+
+        
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
@@ -45,9 +63,18 @@ const OnBoardingPage = () => {
     </Card>
 
     
-    <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-large">For your brand</h4>
+    <Card className="py-4"  >
+    <Checkbox className='pl-4 pt-4' isSelected={type == 2} onValueChange={()=>setType(2)}></Checkbox>
+      <CardHeader className="pb-0 pt-2 px-4 flex-col justify-center items-center">
+      <Image
+        alt="Woman listing to music"
+        className="object-cover  m-5  text-center h-32 justify-center"
+        isZoomed
+        isBlurred
+        height={50}
+        src={type2.src}
+      />
+        <h4 className="font-bold text-large pt-4">For Your Brand</h4>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
@@ -58,9 +85,18 @@ const OnBoardingPage = () => {
         />
       </CardBody>
     </Card>
-    <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-large">For your team</h4>
+    <Card className="py-4" >
+      <Checkbox className='pl-4 pt-4' isSelected={type == 3} onValueChange={()=>setType(3)}></Checkbox>
+      <CardHeader className="pb-0 pt-2 px-4 flex-col justify-center items-center">
+      <Image
+        alt="Woman listing to music"
+        className="object-cover  m-5 h-32 justify-center"
+        isZoomed
+        isBlurred
+        height={50}
+        src={type3.src}
+      />
+        <h4 className="font-bold text-large pt-4">For Your Team</h4>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
@@ -75,7 +111,7 @@ const OnBoardingPage = () => {
    
     </div>
 
-    <Button color='secondary' onClick={()=>router.replace("/users")}> Take me to Blink</Button>
+    <Button color='secondary' onClick={()=>router.replace("/users?type"+{type}+"")}> Take me to Blink</Button>
 
     </div>
     </>
